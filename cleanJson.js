2543157a19d3a0c1997c25aa16e4e8d1c8600699
Juni0r <105945007+swapper@users.noreply.github.com>
2022-08-16 11:39:47 +0100

    Update cleanJson.js

--- cleanJson.js
+++ cleanJson.js
@@ -9,20 +9,9 @@ const emp0 = {
     hourlyIncome: "£15.62",
     jobRole: "Junior Developer"
 }
-const emp1 = {
-    name: "Sophie",
-    lastName: "Clark",
-    age: 29,
-    employeeID: 3242325234992,
-    hourlyIncome: "£11.60",
-    jobRole: "Apprentice"
-}
-
 fs.writeFileSync(
     'output.txt',
     JSON.stringify(emp1, null, 3),
-    JSON.stringify(emp0, null, 3),
-    'utf-8',
     'utf-8',
 
 )
